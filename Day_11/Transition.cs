e13af4201651b1cfb9767feb9a7f6ad595e9902e
Julius Heins <Julius.JH.Heins@bmw.de>
2018-11-13 15:29:14 +0100

    Refactor

---
+++ Day_11/Transition.cs
@@ -0,0 +1,14 @@
+﻿namespace Day_11
+{
+    public struct Transition
+    {
+        public int Direction;
+        public int[] ItemIndicies;
+
+        public Transition(int direction, int[] itemIndicies)
+        {
+            Direction = direction;
+            ItemIndicies = itemIndicies;
+        }
+    }
+}
