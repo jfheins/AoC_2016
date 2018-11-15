fda0d849a80ed4dfe18ed868cf66d229efc82c9d
Julius Heins <Julius.JH.Heins@bmw.de>
2018-11-15 17:59:09 +0100

    Started Day 16

---
+++ Day_16/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Day_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var seed = "11111";
+
+            Console.WriteLine(Enlarge(seed));
+            Console.ReadLine();
+        }
+
+
+        public static string Enlarge(string a)
+        {
+            var b = string.Concat(a.Reverse().Select(c => (char)('1' - c)));
+            return string.Concat(a, "0", b);
+        }
+    }
+}
