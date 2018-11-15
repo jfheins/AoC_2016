892830154148b8cf20565e2ffd814f9739b5b722
Julius Heins <Julius.JH.Heins@bmw.de>
2018-11-15 12:55:52 +0100

    Implement search

--- Day_11/Program.cs
+++ Day_11/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Security.Cryptography.X509Certificates;
@@ -16,20 +17,41 @@ namespace Day_11
             input = "1 1 1 2 3 2 3 2 3 2 3";
             var initialState = State.FromString(input);
 
-            var visitedStates = new Dictionary<State, int>();
+            var sw = new Stopwatch();
 
             Console.WriteLine("Initial State:");
             Console.WriteLine(initialState);
 
-            Path finalPath = null;
-			
+            sw.Start();
 
 
+            var visitedStates = new HashSet<State>();
+            var nextStates = new HashSet<State>() { initialState };
 
+            var stepCounter = 0;
+            while (nextStates.Any())
+            {
+                stepCounter++;
+                Console.Write($"Step {stepCounter}, expanding {nextStates.Count} nodes ... ");
 
-			Console.WriteLine($"Final state reached after {finalPath.Length} steps :-)");
+                var expanded = nextStates.AsParallel().SelectMany(s => s.GetPossibleSuccessorStates());
+                nextStates = new HashSet<State>(expanded);
 
-			Console.ReadLine();
+                Console.WriteLine($"into {nextStates.Count} unique new nodes.");
+
+                if (nextStates.AsParallel().Any(s => s.IsFinalState()))
+                    break;
+                
+                visitedStates.UnionWith(nextStates);
+
+            }
+            sw.Stop();
+            var duration = sw.ElapsedMilliseconds / 1000f;
+
+            Console.WriteLine("========================================");
+			Console.WriteLine($"Final state reached after {stepCounter} steps :-)");
+            Console.WriteLine($"It took {duration:0.000} seconds.");
+            Console.ReadLine();
         }
     }
 }
