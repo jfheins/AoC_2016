d93fb7261f351bafc51ff87ffb88e45342663ef2
Julius Heins <Julius.JH.Heins@bmw.de>
2018-11-16 09:45:18 +0100

    Part 2

--- Day_16/Program.cs
+++ Day_16/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Day_16
 {
@@ -8,10 +9,16 @@ namespace Day_16
         {
             var seed = "10011111011011001";
             var diskLength = 272;
+            diskLength = 35651584;
 
+            var sw = new Stopwatch();
+            sw.Start();
             var diskContent = StringEnlarger.EnlargeToLength(seed, diskLength);
             var checksum = StringEnlarger.CalculateChecksum(diskContent);
+            sw.Stop();
+
             Console.WriteLine($"Checksum for seed {seed} expanded to {diskLength} is {checksum}.");
+            Console.WriteLine($"Calculation took {sw.ElapsedMilliseconds}ms.");
 
             Console.ReadLine();
         }
