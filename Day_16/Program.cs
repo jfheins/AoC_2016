23b532ac14cc1a755849b9282d3e29974becd113
Julius Heins <Julius.JH.Heins@bmw.de>
2018-11-16 12:17:39 +0100

    Added char at pos method

--- Day_16/Program.cs
+++ Day_16/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Day_16
 {
@@ -9,7 +10,7 @@ namespace Day_16
         {
             var seed = "10011111011011001";
             var diskLength = 272;
-            diskLength = 35651584;
+            //diskLength = 35651584;
 
             var sw = new Stopwatch();
             sw.Start();
@@ -17,6 +18,16 @@ namespace Day_16
             var checksum = StringEnlarger.CalculateChecksum(diskContent);
             sw.Stop();
 
+            //Console.WriteLine($"Disk content for seed {seed} expanded to {diskLength} is:");
+            Console.WriteLine(diskContent);
+            Console.WriteLine($"Checksum for seed {seed} expanded to {diskLength} is {checksum}.");
+            Console.WriteLine($"Calculation took {sw.ElapsedMilliseconds}ms.");
+
+            sw.Restart();
+            var expContent = Enumerable.Range(0, diskLength).Select(x => StringEnlarger.CharAtPosition(seed, diskLength, x));
+            checksum = StringEnlarger.CalculateChecksum(expContent);
+
+            Console.WriteLine(string.Concat(expContent));
             Console.WriteLine($"Checksum for seed {seed} expanded to {diskLength} is {checksum}.");
             Console.WriteLine($"Calculation took {sw.ElapsedMilliseconds}ms.");
 
--- Day_16/StringEnlarger.cs
+++ Day_16/StringEnlarger.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Day_16
@@ -22,7 +23,43 @@ namespace Day_16
             return result.Substring(0, length);
         }
 
-        public static string CalculateChecksum(string content)
+        private static string b;
+
+        public static char CharAtPosition(string seed, int contentLength, int position)
+        {
+            if (position >= contentLength)
+            {
+                throw new ArgumentException("Position too large");
+            }
+            if (contentLength <= seed.Length)
+            {
+                // No expansion has taken place
+                return seed[position];
+            }
+
+            var blockNumber = Math.DivRem(position, seed.Length + 1, out var posInBlock);
+
+            if (posInBlock == seed.Length)
+            {
+                // Dragon bit
+                return '_';
+            }
+            else
+            {
+                if (blockNumber % 2 == 0)
+                {
+                    // 'a' block
+                    return seed[posInBlock];
+                }
+                else
+                {
+                    b = b ?? string.Concat(seed.Reverse().Select(c => (char) ('a' - c)));
+                    return b[posInBlock];
+                }
+            }
+        }
+
+        public static string CalculateChecksum(IEnumerable<char> content)
         {
             var checksum = string.Concat(content.Pairwise().Select(ChecksumBitFromPair));
             if (checksum.Length % 2 == 0)
