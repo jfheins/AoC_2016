139bac1f05c0f8e7bb230ba70269e32fd6bc5dc6
Julius Heins <Julius.JH.Heins@bmw.de>
2018-11-29 10:00:34 +0100

    Start with Day 4

---
+++ Day_04/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+
+namespace Day_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Room
+    {
+        public string Name { get; }
+        public int SectorId { get; }
+        public string Checksum { get; }
+
+        public Room(string name, int sectorId, string checksum)
+        {
+            Name = name;
+            SectorId = sectorId;
+            Checksum = checksum;
+        }
+
+        public bool IsReal()
+        {
+
+        }
+
+        private Dictionary<char, int> NameLettersWithOccurence => Name.
+    }
+}
