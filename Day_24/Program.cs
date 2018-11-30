5b7579221a9f2e90bce122d0153f786e37312780
Julius F. Heins <jfheins@gmail.com>
2018-11-30 20:57:23 +0100

    Day 24. Done, but ugly.

--- Day_24/Program.cs
+++ Day_24/Program.cs
@@ -3,145 +3,184 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Core;
+using Core.Combinatorics;
 
 namespace Day_24
 {
-    internal class Program
-    {
-        private static int rows;
-        private static int columns;
-        private static char[][] field;
+	internal class Program
+	{
+		private static int rows;
+		private static int columns;
+		private static char[][] field;
 
-        private static void Main(string[] args)
-        {
-            var inputText = File.ReadAllLines(@"../../../input.txt");
-            field = inputText.Select(x => x.ToCharArray()).ToArray();
-            rows = field.Length;
-            columns = field[0].Length;
+		private static void Main(string[] args)
+		{
+			var inputText = File.ReadAllLines(@"../../../input.txt");
+			field = inputText.Select(x => x.ToCharArray()).ToArray();
+			rows = field.Length;
+			columns = field[0].Length;
 
-            var allNumbers = field.SelectMany(x => x).Where(char.IsDigit).ToArray();
-            var origin = new Coordinate(1, 1);
+			var allNumbers = field.SelectMany(x => x).Where(char.IsDigit).ToArray();
 
-            var bfs = new BreadthFirstSearch<Coordinate, MoveDirection>(new CoordinateIdentical(), GetPossibleMovements,
-                GetPosition);
+			var numberPositions = new Dictionary<char, Coordinate>();
 
-            var temp = bfs.FindAll(origin, coords => char.IsDigit(ContentAt(coords)));
-            var numberPositions = new Dictionary<char, Coordinate>();
-            foreach (var path in temp)
-            {
-                var item = ContentAt(path[0]);
-                Console.WriteLine($"Item {item} found at {path[0]}");
-                numberPositions.Add(item, path[0]);
-            }
+			for (var rowIndex = 0; rowIndex < rows; rowIndex++)
+			for (var colIndex = 0; colIndex < columns; colIndex++)
+			{
+				var c = new Coordinate(colIndex, rowIndex);
+				var content = ContentAt(c);
+				if (char.IsDigit(content))
+					numberPositions.Add(content, c);
+			}
 
-            var distances = new List<ValueTuple<char, char, int>>();
+			foreach (var position in numberPositions)
+			{
+				Console.WriteLine($"Number {position.Key} found at {position.Value}");
+			}
 
-            foreach (var number in numberPositions.Keys)
-            {
-                var others = bfs.FindAll(numberPositions[number], coords => ContentAt(coords) > number);
-                foreach (var other in others)
-                    distances.Add((number, ContentAt(other[0]), other.Length));
-            }
+			var bfs = new BreadthFirstSearch<Coordinate, MoveDirection>(new CoordinateIdentical(), GetPossibleMovements,
+				GetPosition);
 
-            Console.ReadLine();
-        }
+			var distances = new Dictionary<string, int>();
+			foreach (var number in numberPositions.Keys)
+			{
+				var others = bfs.FindAll(numberPositions[number], coords => ContentAt(coords) > number);
+				foreach (var other in others)
+				{
+					var edge1 = string.Concat(number, ContentAt(other[0]));
+					var edge2 = string.Concat(ContentAt(other[0]), number);
+					var edgeLength = other.Length - 1;
+					distances.Add(edge1, edgeLength);
+					distances.Add(edge2, edgeLength);
+					Console.WriteLine($"Edge {edge1} / {edge2} is {edgeLength} long.");
+				}
+			}
 
-        private static bool IsOtherNumber(Coordinate arg, char self)
-        {
-            var target = ContentAt(arg);
-            return target != self && char.IsDigit(target);
-        }
+			var allTrips = new Permutations<char>(allNumbers);
 
-        private static char ContentAt(Coordinate pos)
-        {
-            return field[pos.Y][pos.X];
-        }
+			var allTripsWithLengths = new Dictionary<string, int>();
 
-        private static Coordinate GetPosition(Coordinate position, MoveDirection direction)
-        {
-            var newpos = position;
-            switch (direction)
-            {
-                case MoveDirection.Up:
-                    newpos = new Coordinate(position.X, position.Y - 1);
-                    break;
-                case MoveDirection.Down:
-                    newpos = new Coordinate(position.X, position.Y + 1);
-                    break;
-                case MoveDirection.Left:
-                    newpos = new Coordinate(position.X - 1, position.Y);
-                    break;
-                case MoveDirection.Right:
-                    newpos = new Coordinate(position.X + 1, position.Y);
-                    break;
-            }
+			foreach (IList<char> trip in allTrips)
+			{
+				if (trip[0] != '0')
+					continue;
+				trip.Add('0');
+				var name = string.Concat(trip);
+				var length = 0;
+				foreach (var walk in trip.PairwiseWithOverlap())
+				{
+					var edge = string.Concat(walk.Item1, walk.Item2);
+					length += distances[edge];
+				}
 
-            return newpos.IsValid() ? newpos : null;
-        }
+				allTripsWithLengths.Add(name, length);
+			}
 
-        private static IEnumerable<MoveDirection> GetPossibleMovements(Coordinate position)
-        {
-            if (position == null)
-                yield break;
-            if (position.X > 0)
-                yield return MoveDirection.Left;
+			Console.WriteLine();
 
-            if (position.Y > 0)
-                yield return MoveDirection.Up;
+			var min = allTripsWithLengths.OrderBy(x => x.Value).Take(3);
+			foreach (var trip in min)
+				Console.WriteLine($"Trip {trip.Key} is {trip.Value} long");
 
-            if (position.X < columns - 1)
-                yield return MoveDirection.Right;
+			Console.ReadLine();
+		}
 
-            if (position.Y < rows - 1)
-                yield return MoveDirection.Down;
-        }
+		private static bool IsOtherNumber(Coordinate arg, char self)
+		{
+			var target = ContentAt(arg);
+			return target != self && char.IsDigit(target);
+		}
 
-        public class Coordinate
-        {
-            public Coordinate(int x, int y)
-            {
-                X = x;
-                Y = y;
-            }
+		private static char ContentAt(Coordinate pos)
+		{
+			return field[pos.Y][pos.X];
+		}
 
-            public int X { get; }
-            public int Y { get; }
+		private static Coordinate GetPosition(Coordinate position, MoveDirection direction)
+		{
+			var newpos = position;
+			switch (direction)
+			{
+				case MoveDirection.Up:
+					newpos = new Coordinate(position.X, position.Y - 1);
+					break;
+				case MoveDirection.Down:
+					newpos = new Coordinate(position.X, position.Y + 1);
+					break;
+				case MoveDirection.Left:
+					newpos = new Coordinate(position.X - 1, position.Y);
+					break;
+				case MoveDirection.Right:
+					newpos = new Coordinate(position.X + 1, position.Y);
+					break;
+			}
 
-            public bool IsValid()
-            {
-                return ContentAt(this) != '#';
-            }
+			return newpos.IsValid() ? newpos : null;
+		}
 
-            public override string ToString()
-            {
-                return $"({X}|{Y})";
-            }
-        }
+		private static IEnumerable<MoveDirection> GetPossibleMovements(Coordinate position)
+		{
+			if (position == null)
+				yield break;
+			if (position.X > 0)
+				yield return MoveDirection.Left;
 
-        internal class CoordinateIdentical : EqualityComparer<Coordinate>
-        {
-            public override bool Equals(Coordinate a, Coordinate b)
-            {
-                if (a == null && b == null)
-                    return true;
-                if (a == null || b == null)
-                    return false;
+			if (position.Y > 0)
+				yield return MoveDirection.Up;
 
-                return a.X == b.X && a.Y == b.Y;
-            }
+			if (position.X < columns - 1)
+				yield return MoveDirection.Right;
 
-            public override int GetHashCode(Coordinate point)
-            {
-                return HashCode.Combine(point.X, point.Y);
-            }
-        }
-    }
+			if (position.Y < rows - 1)
+				yield return MoveDirection.Down;
+		}
 
-    public enum MoveDirection
-    {
-        Up,
-        Down,
-        Left,
-        Right
-    }
+		public class Coordinate
+		{
+			public Coordinate(int x, int y)
+			{
+				X = x;
+				Y = y;
+			}
+
+			public int X { get; }
+			public int Y { get; }
+
+			public bool IsValid()
+			{
+				return ContentAt(this) != '#';
+			}
+
+			public override string ToString()
+			{
+				return $"({X}|{Y})";
+			}
+		}
+
+		internal class CoordinateIdentical : EqualityComparer<Coordinate>
+		{
+			public override bool Equals(Coordinate a, Coordinate b)
+			{
+				if (a == null && b == null)
+					return true;
+				if (a == null || b == null)
+					return false;
+
+				return a.X == b.X && a.Y == b.Y;
+			}
+
+			public override int GetHashCode(Coordinate point)
+			{
+				return HashCode.Combine(point.X, point.Y);
+			}
+		}
+	}
+
+	public enum MoveDirection
+	{
+		Up,
+		Down,
+		Left,
+		Right
+	}
 }
